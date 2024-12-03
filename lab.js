df9b59056381d4f8ac1f7ce604f7503cae54ed9d
Elena-Abreu <ena3@njit.edu>
2024-12-03 11:14:47 -0500

    Question 2

--- lab.js
+++ lab.js
@@ -3,4 +3,22 @@ function greetUser()
     let firstName = 'Harry' 
 document.getElementById('output').innerText = "Hello, " + firstName;
 }
-greetUser()
+greetUser() 
+
+function GreetButton() 
+{    
+
+
+document.getElementById('message').innerText = "Hello, World!";
+} 
+GreetButton() 
+
+
+function AddNumbers(username, lastname)  
+{
+    let a = 100 ;
+    let b = 50 ;
+    //console.log(a+b);    only necessary if no getelementbyid
+    document.getElementById('heading-two').innerText = (a + b); 
+    document.getElementById('para1').innerHTML = "<h1>Para changes to H1 Heading<h1>"; } 
+AddNumbers()
